75fbf1efc204e93324d66f3d79a8c92f83007d54
Hexalantes <134841282+Hexalantes@users.noreply.github.com>
2025-07-26 16:27:56 +0300

    Update main.py

--- airootfs/etc/calamares/main.py
+++ airootfs/etc/calamares/main.py
@@ -1,73 +1,81 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+#
 
 import os
 import subprocess
 import libcalamares
-from libcalamares.utils import check_target_env_call, check_target_env_output
+
+def run_grub_install(fw_type, efi_dir, install_hybrid):
+    partitions = libcalamares.globalstorage.value("partitions")
+    if not partitions:
+        libcalamares.utils.warning("Partition list empty, cannot install bootloader.")
+        return
+
+    if fw_type == "efi":
+        efi_bootloader_id = libcalamares.job.configuration.get("bootloaderEntryName", "GRUB")
+        target = "x86_64-efi"  # çoğunlukla AMD64 sistemler için
+        # grub-install komutu
+        cmd = [
+            "grub-install",
+            "--target=" + target,
+            "--efi-directory=" + efi_dir,
+            "--bootloader-id=" + efi_bootloader_id,
+            "--recheck",
+            "--force"
+        ]
+        libcalamares.utils.debug(f"Running: {' '.join(cmd)}")
+        libcalamares.utils.target_env_call(cmd)
+
+    elif fw_type == "bios":
+        # BIOS için disk yolunu bul
+        boot_disk = None
+        efi_system_partition = libcalamares.globalstorage.value("efiSystemPartition")
+        if efi_system_partition:
+            # Örnek: /boot/efi mount noktası varsa, bağlı disk tespiti
+            find_disk_cmd = f"lsblk -no PKNAME $(df --output=source {efi_system_partition} | tail -n1)"
+            disk_name = libcalamares.utils.target_env_output(["sh", "-c", find_disk_cmd]).strip()
+            if disk_name:
+                boot_disk = "/dev/" + disk_name
+
+        if not boot_disk:
+            # Eğer disk bulunamazsa fallback olarak /dev/sda alalım
+            boot_disk = "/dev/sda"
+
+        cmd = [
+            "grub-install",
+            "--target=i386-pc",
+            "--recheck",
+            "--force",
+            boot_disk
+        ]
+        libcalamares.utils.debug(f"Running: {' '.join(cmd)}")
+        libcalamares.utils.target_env_call(cmd)
+
+    else:
+        libcalamares.utils.warning(f"Firmware type {fw_type} not supported for grub install.")
+
+
+def run_grub_mkconfig():
+    output_cfg = "/boot/grub/grub.cfg"
+    grub_mkconfig = libcalamares.job.configuration.get("grubMkconfig", "grub-mkconfig")
+    cmd = [grub_mkconfig, "-o", output_cfg]
+    libcalamares.utils.debug(f"Running: {' '.join(cmd)}")
+    libcalamares.utils.target_env_call(cmd)
+
 
 def run():
     fw_type = libcalamares.globalstorage.value("firmwareType")
-    efi_boot_loader = libcalamares.job.configuration.get("efiBootLoader", "grub")
-    efi_directory = libcalamares.globalstorage.value("efiSystemPartition")
-    partitions = libcalamares.globalstorage.value("partitions")
+    efi_dir = libcalamares.globalstorage.value("efiSystemPartition")
+    install_hybrid = libcalamares.job.configuration.get("installHybridGRUB", False)
 
-    if not partitions:
-        return ("Bootloader Error", "No partitions defined.")
-
-    if fw_type == "efi":
-        return install_grub_uefi(efi_directory)
-    else:
-        return install_grub_bios()
-
-def install_grub_uefi(efi_directory):
-    root_mount = libcalamares.globalstorage.value("rootMountPoint")
-    efi_mount = os.path.join(root_mount, efi_directory.lstrip("/"))
-
-    if not os.path.exists(efi_mount):
-        os.makedirs(efi_mount)
-
-    cmd = [
-        "grub-install",
-        "--target=x86_64-efi",
-        f"--efi-directory={efi_mount}",
-        "--bootloader-id=PardusTheArchean",
-        "--recheck",
-        "--no-nvram",
-        "--removable"
-    ]
+    libcalamares.utils.debug(f"Firmware type: {fw_type}, EFI directory: {efi_dir}")
 
     try:
-        check_target_env_call(cmd)
+        run_grub_install(fw_type, efi_dir, install_hybrid)
+        run_grub_mkconfig()
     except subprocess.CalledProcessError as e:
-        return ("GRUB EFI Install Failed", str(e))
-
-    return generate_grub_cfg()
-
-def install_grub_bios():
-    device = libcalamares.globalstorage.value("bootLoaderInstallPath")
-    if not device:
-        return ("Bootloader Error", "No BIOS install device specified.")
-
-    cmd = [
-        "grub-install",
-        "--target=i386-pc",
-        f"--boot-directory=/boot",
-        "--recheck",
-        device
-    ]
-
-    try:
-        check_target_env_call(cmd)
-    except subprocess.CalledProcessError as e:
-        return ("GRUB BIOS Install Failed", str(e))
-
-    return generate_grub_cfg()
-
-def generate_grub_cfg():
-    try:
-        check_target_env_call(["grub-mkconfig", "-o", "/boot/grub/grub.cfg"])
-    except subprocess.CalledProcessError as e:
-        return ("GRUB Config Error", str(e))
-
+        libcalamares.utils.warning(f"Bootloader install failed: {e}")
+        return ("Bootloader installation error",
+                f"Command {e.cmd} failed with exit code {e.returncode}")
     return None
