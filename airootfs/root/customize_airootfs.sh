6046c480617c67c6faf32650d2689033fd285a08
Hexalantes <134841282+Hexalantes@users.noreply.github.com>
2025-07-26 07:12:48 +0300

    Update customize_airootfs.sh

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -3,11 +3,7 @@
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 
 useradd -m liveuser
-echo "liveuser:zen" | chpasswd
-
-bsdtar -xpf /etc/parduspkg/ckbcomp-1.239-1-any.pkg.tar.zst -C /
-bsdtar -xpf /etc/parduspkg/calamares-git-3.3.14.r25.g95aa33f-1-x86_64.pkg.tar.zst -C /
-bsdtar -xpf /etc/parduspkg/neofetch-git-7.1.0.r166.gccd5d9f5-1-any.pkg.tar.zst -C /
+echo "liveuser:pardus" | chpasswd
 
 echo "liveuser ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/liveuser
 chmod 440 /etc/sudoers.d/liveuser
@@ -47,8 +43,8 @@ systemctl enable NetworkManager
 rm -f /etc/lsb-release
 cat <<EOF > /etc/lsb-release
 DISTRIB_ID="Pardus The Archean"
-DISTRIB_RELEASE="Beta 3"
-DISTRIB_DESCRIPTION="Pardus The Archean Beta 3"
+DISTRIB_RELEASE="Milestone 1"
+DISTRIB_DESCRIPTION="Pardus The Archean Milestone 1"
 EOF
 
 mkdir -p /home/liveuser/.config/autostart
